f7eb08f15b4825d325dca94ecd96f18b3b159fa3
Lauren Bentley <laurendebentley@gmail.com>
2023-02-03 15:29:26 -0500

    added unique id to added notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+const { randomUUID } = require('crypto');
 const { response } = require('express');
 const express = require('express');
 const fs = require('fs');
@@ -5,6 +6,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001
 const db = require('./db/db.json')
+const uuid = require('./public/assets/js/uuid')
 
 
 
@@ -34,4 +36,19 @@ res.sendFile(path.join(__dirname, 'public/index.html'));
   app.get('/api/notes', (req, res) => {
     res.json(db)
   })
+
+app.post(`/api/notes`, (req, res) => {
+    const title = req.body.title
+    const text = req.body.text
+    // Creates an object to be added to json file
+    const newNote = {
+        title,
+        text,
+        id: uuid()
+    }
+    // Takes the newly created object (note) and moves it to our db.json file
+    db.push(newNote)
+    fs.writeFile(`./db/db.json`, JSON.stringify(db), (err) => err ? console.error(err) : console.log(`New Note created`))
+    res.json(db)
+})
 
